8bd9c4d59930c3b9557e2b364ba8cd33e2281176
Charan Teja <charan.teja@lumo.ai>
2026-08-13 16:00:05 +0530

    Fix entrypoint to serve LUMO homepage

--- etc/tool/openapi.py
+++ etc/tool/openapi.py
@@ -4,8 +4,10 @@ from g4f.api import create_app
 
 app = create_app()
 
-with open("openapi.json", "w") as f:
-    data = json.dumps(app.openapi())
-    f.write(data)
-
-print(f"openapi.json - {round(len(data)/1024, 2)} kbytes")
+try:
+    with open("openapi.json", "w") as f:
+        data = json.dumps(app.openapi())
+        f.write(data)
+    print(f"openapi.json - {round(len(data)/1024, 2)} kbytes")
+except Exception as e:
+    print(f"Loaded FastAPI app for uvicorn deployment: {e}")
